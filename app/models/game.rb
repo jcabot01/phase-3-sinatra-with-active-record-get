8b331dcdd0105464ed0ed8b8f47f5e3ab4343d76
JonCabot <jfc0053@gmail.com>
2022-04-23 08:33:51 -0500

    commit tree 19177f4911dee6d6222200673c0cd127606079a2

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,5 @@
 class Game < ActiveRecord::Base
+  #associated models
   has_many :reviews
   has_many :users, through: :reviews
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ActiveRecord::Base
+  #associated models
   belongs_to :game
   belongs_to :user
 end
